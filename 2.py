e8a978270bca6f06abed93c367446af5d6e84f0b
Gunch85 <gunch1985@gmail.com>
2022-07-13 15:33:41 +0300

    random

--- 2.py
+++ 2.py
@@ -1,5 +1,13 @@
-import random
+# import random
+#
+# a = random.randint(100, 999)
+# print('Случайное число =', a)
+# print(a // 100 + a // 10 % 10 + a % 10)
 
-a = random.randint(100, 999)
-print('Случайное число =', a)
-print(a // 100 + a // 10 % 10 + a % 10)
+import random
+num = random.randint(100, 999)
+print(num)
+a = num // 100
+b = (num // 10) % 10
+c = num % 10
+print(a + b + c)
